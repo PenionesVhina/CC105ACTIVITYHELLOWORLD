cb1bd1499883251decbd8f23786098a68c9da9ed
Vhina <sm201600436@wmsu.edu.ph>
2020-10-18 12:01:19 +0800

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 //Peniones
-
-/*
 //Hello World
 void main() => runApp(
       MaterialApp(
@@ -11,25 +9,6 @@ void main() => runApp(
       ),
     );
 
-*/
 
-//Scaffold
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.black38,
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://s3.amazonaws.com/images.seroundtable.com/google-css-images-1515761601.jpg'),
-          ),
-        ),
-      ),
-    ),
-  );
-}
+
+
