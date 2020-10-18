8e63740631c5ed0658bca08df90474f8eaded86a
Vhina <sm201600436@wmsu.edu.ph>
2020-10-18 11:21:13 +0800

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 //Peniones
 
 //Hello World
-/*
 void main() => runApp(
       MaterialApp(
         home: Center(
@@ -11,8 +10,8 @@ void main() => runApp(
       ),
     );
 
-*/
 
+/*
 //Scaffold
 void main() {
   runApp(
@@ -33,3 +32,4 @@ void main() {
     ),
   );
 }
+*/
